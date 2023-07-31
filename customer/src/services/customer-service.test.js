5e4fb6370c2a17f01d516de60d56b8d2d9b14c8b
Nes224 <Nes224@hotmail.com>
2023-07-31 10:52:38 +0700

    Added test Cases Configurations

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,10 @@
+// which service it is
+describe("customerService", () => {
+    // Which function
+    describe("SignIn", () => {
+        // Which Scenario we are testing
+        test("validate user inputs", () => { });
+
+        test("validate response", () => { });
+    });
+});
